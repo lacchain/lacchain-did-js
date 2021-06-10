46d7c14b8a83a7d7d0f6dc6b54c7c872f3946b3e
Sergio Ceron <sceronf@gmail.com>
2021-06-09 19:44:39 -0500

    Fix import module

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
-export * as DID from './lib/did.js';
-export * as DIDRecoverable from './lib/didrecoverable.js';
-export * as Resolver from './lib/resolver.js';
+export { default as DID } from './lib/did.js';
+export { default as DIDRecoverable } from './lib/didrecoverable.js';
+export { getResolver as Resolver } from './lib/resolver.js';
