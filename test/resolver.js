37b7e71778683cd0c7b7ab92ff5e0636451fe339
Sergio Ceron <sceronf@gmail.com>
2023-03-17 11:39:52 -0600

    Fix import

--- test/resolver.js
+++ test/resolver.js
@@ -8,13 +8,15 @@ chai.use( chaiAsPromised );
 chai.should();
 
 describe( 'DIDResolver', () => {
-	const did = 'did:lac:main:0xcd7ebd413d512b47d1d48e5ed27fe01c8c29fd98';
+	const did = 'did:lac:omega:0xcd7ebd413d512b47d1d48e5ed27fe01c8c29fd98';
 	const invalid = 'dsid:lac:axcd7ebd413d512b47d1d48e5ed27fe01c8c29fd98';
 	const resolver = getResolver( {
 		networks: [{
-			name: 'main',
-			registry: '0xDF1EbaE3e2318AA57CFaf11E1Dd3531D5A5AdA04',
-			rpcUrl: 'https://writer.lacchain.net',
+			name: 'omega',
+			registry: '0xde82d4Cea1242C04B6baF6f98095F6050c88f50b',
+			rpcUrl: 'http://34.73.228.200',
+			nodeAddress: '0x971bb94d235a4ba42d53ab6fb0a86b12c73ba460',
+			expiration: '1736394529',
 		}]
 		// mode: 'explicit'
 	} );
