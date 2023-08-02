40a15856d18c318de45cb15a2706c1cf18753ad2
eum602 <eum602@gmail.com>
2023-08-02 11:00:08 -0500

    refactor: move constants

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 export { default as DID } from "./lib/did.js";
+export { default as Lac1DID } from "./lib/lac1/lac1Did.js";
 export { default as DIDRecoverable } from "./lib/didrecoverable.js";
 export { getResolver as Resolver } from "./lib/resolver.js";
 export { getResolver as Lac1Resolver } from "./lib/lac1/resolver.js";
